fb4bfa517689c24347f794a39d82828dc7fdeace
1A291109A6 <128209125+1A291109A6@users.noreply.github.com>
2023-03-19 22:18:54 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,26 +15,26 @@ function init() {
     // ファイルを読み込んだ時に呼び出される関数
     reader.onload = (event) => {
       // Web Audio APIを使用して、wavファイルのデータを解析
-      const audioContext = new AudioContext();
-      audioContext.decodeAudioData(event.target.result, (buffer) => {
-        const channelData = buffer.getChannelData(0);
-        let textData = '';
-        for (let i = 0; i < channelData.length; i++) {
-          // チャンネルデータをテキストに変換
-          textData += channelData[i].toString() + '\n';
-        }
-        
-        // ダウンロード用のリンクを作成
-        const blob = new Blob([textData], { type: 'text/plain' });
-        const url = URL.createObjectURL(blob);
-        downloadLink.href = url;
-        downloadLink.download = 'wave_data.txt';
-        
-        // ダウンロードリンクをクリックして、自動ダウンロードする
-        downloadLink.click();
-      });
-    };
-    
+const audioContext = new AudioContext();
+audioContext.decodeAudioData(event.target.result, (buffer) => {
+  const channelData = buffer.getChannelData(0);
+  let textData = '';
+  const samplingRate = buffer.sampleRate;
+  const step = samplingRate / 8000;
+  for (let i = 0; i < channelData.length; i += step) {
+    // チャンネルデータをテキストに変換
+    textData += channelData[Math.floor(i)].toString() + '\n';
+  }
+
+  // ダウンロード用のリンクを作成
+  const blob = new Blob([textData], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+  downloadLink.href = url;
+  downloadLink.download = 'wave_data.txt';
+
+  // ダウンロードリンクをクリックして、自動ダウンロードする
+  downloadLink.click();
+});
     // ファイルを読み込む
     reader.readAsArrayBuffer(file);
   });
