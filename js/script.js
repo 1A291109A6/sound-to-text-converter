860ad1c60c600f293d4ad70e4ccbe5cf6274ecb0
1A291109A6 <128209125+1A291109A6@users.noreply.github.com>
2023-03-25 23:44:03 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,47 @@
-// ページの読み込みが完了した時に呼び出される関数
 function init() {
-  // HTMLの要素を取得
   const fileInput = document.getElementById('fileInput');
   const downloadLink = document.getElementById('downloadLink');
-  
-  // ファイルが選択されたときに呼び出される関数
+
   fileInput.addEventListener('change', (event) => {
-    // 選択されたファイルを取得
     const file = event.target.files[0];
-    
-    // FileReaderオブジェクトを作成
     const reader = new FileReader();
-    
-    // ファイルを読み込んだ時に呼び出される関数
+
     reader.onload = (event) => {
-      // Web Audio APIを使用して、wavファイルのデータを解析
       const audioContext = new AudioContext();
-      audioContext.decodeAudioData(event.target.result, (buffer) => {
+
+      let decodeFunction;
+      switch (file.type) {
+        case 'audio/wav':
+          decodeFunction = audioContext.decodeAudioData;
+          break;
+        case 'audio/mp3':
+        case 'audio/mpeg':
+          decodeFunction = audioContext.decodeAudioData.bind(audioContext, event.target.result);
+          break;
+        case 'audio/ogg':
+          decodeFunction = audioContext.decodeAudioData.bind(audioContext, event.target.result);
+          break;
+        default:
+          throw new Error(`Unsupported file type: ${file.type}`);
+      }
+
+      decodeFunction((buffer) => {
         const channelData = buffer.getChannelData(0);
         let textData = '';
         const samplingRate = buffer.sampleRate;
-        const step = samplingRate / 8000;
+        const step = samplingRate;
         for (let i = 0; i < channelData.length; i += step) {
-          // チャンネルデータをテキストに変換
           textData += channelData[Math.floor(i)].toString() + '\n';
         }
 
-        // ダウンロード用のリンクを作成
         const blob = new Blob([textData], { type: 'text/plain' });
         const url = URL.createObjectURL(blob);
         downloadLink.href = url;
         downloadLink.download = 'wave_data.txt';
-
-        // ダウンロードリンクをクリックして、自動ダウンロードする
         downloadLink.click();
       });
     }
-    
-    // ファイルを読み込む
+
     reader.readAsArrayBuffer(file);
   });
 }
